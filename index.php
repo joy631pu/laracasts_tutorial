95de918074bb0cbbf9c83ea79c657c5872abd74f
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-09-11 02:03:10 -0700

    Add conditionals and booleans

--- index.php
+++ index.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+        body{
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
         <?php 
-        $greeting = "Hello";
-        echo "$greeting, Everybody";
+        $name = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+           $message = "You have read $name";
+        } else {
+            $message = "You have NOT read $name";
+        }
         ?>
+    <h1>
+    <?= $message ?>     
         
     </h1>
     
