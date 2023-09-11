e35b11e2fe44b33fe546850a27102934ab974fa8
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-09-11 02:23:02 -0700

    Add array

--- index.php
+++ index.php
@@ -4,31 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
-    <style>
-        body{
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
+    
 </head>
 <body>
-        <?php 
-        $name = "Dark Matter";
-        $read = true;
+<h1>Recommended Books</h1>
 
-        if ($read) {
-           $message = "You have read $name";
-        } else {
-            $message = "You have NOT read $name";
-        }
-        ?>
-    <h1>
-    <?= $message ?>     
-        
-    </h1>
+<?php
+    $books = [
+        "Do Androids Dream of Electric Sheep",
+        "The Langoliers",
+        "Hail Mary"
+    ];
+?>
+
+<ul>
+    <?php foreach ($books as $book) : ?>
+        <li><?= $book ?></li>
+    <?php endforeach; ?>
+</ul>
     
 </body>
 </html>
