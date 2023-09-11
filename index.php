18581996322729cc48910bac46e56faae7bdf807
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-09-11 02:27:22 -0700

    Include associative array

--- index.php
+++ index.php
@@ -7,21 +7,32 @@
     
 </head>
 <body>
-<h1>Recommended Books</h1>
-
 <?php
-    $books = [
-        "Do Androids Dream of Electric Sheep",
-        "The Langoliers",
-        "Hail Mary"
-    ];
-?>
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ]
+        ];
+    ?>
 
-<ul>
-    <?php foreach ($books as $book) : ?>
-        <li><?= $book ?></li>
-    <?php endforeach; ?>
-</ul>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
     
 </body>
 </html>
