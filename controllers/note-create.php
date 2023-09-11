9a495f6d46cfcdb74199a477c5655bbe5b26b22f
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-09-11 16:53:28 -0700

    Extract validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,6 +1,7 @@
 <?php
 
-$config = require('config.php');
+require 'Validator.php';
+$config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = 'Create Note';
@@ -8,14 +9,9 @@ $heading = 'Create Note';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'A body is required';
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
     }
-
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'The body can not be more than 1,000 characters.';
-    }
-
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
             'body' => $_POST['body'],
@@ -25,3 +21,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 require 'views/note-create.view.php';
+?>
