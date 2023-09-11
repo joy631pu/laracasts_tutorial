995de86860d11a68506431ec4e4423918d72422d
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-09-11 01:30:23 -0700

    Include variable

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Demo</title>
 </head>
 <body>
     <h1>
         <?php 
-        echo "Hello, World";
+        $greeting = "Hello";
+        echo "$greeting, Everybody";
         ?>
         
     </h1>
